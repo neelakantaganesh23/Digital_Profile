4a0f43af6a8692ee06a7bd7fa1c07161fde490f9
Ganesh Neelakanta <130562632+neelakantaganesh23@users.noreply.github.com>
2025-06-11 14:29:36 +0530

    Update app_l.py

--- app_l.py
+++ app_l.py
@@ -11,7 +11,7 @@ from huggingface_hub import InferenceClient
 
 # Load environment variables from .env file
 load_dotenv()
-HUGGING_FACE_HUB_TOKEN ="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"
+HUGGING_FACE_HUB_TOKEN =os.getenv("HUG")
 # Load Hugging Face Token
 
 if not HUGGING_FACE_HUB_TOKEN:
